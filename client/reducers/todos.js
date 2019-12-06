82e14360adc0a2f1476beff3483f4bb407c3c4c6
Luan <luan.b@stjamesperth.com.au>
2019-12-06 19:52:58 +1100

    Implemented clear complete action in todos reducer by using the filter function to return the state with all todo that have completed === false.

--- client/reducers/todos.js
+++ client/reducers/todos.js
@@ -1,61 +1,65 @@
-import {append, update, updateArrayById} from "./utils";
+import { append, update, updateArrayById } from './utils';
 
 // Dummy data only in the browser
-const initial = typeof window === "undefined" ? [] : [
-  {
-    id: 1,
-    title: "Buy Milk",
-    completed: true
-  },
-  {
-    id: 2,
-    title: "Drink Milk",
-    completed: false
-  },
-  {
-    id: 3,
-    title: "Prepare Workshop",
-    completed: true
-  },
-  {
-    id: 4,
-    title: "Run Successful Workshop",
-    completed: false
-  }
-];
+const initial =
+  typeof window === 'undefined'
+    ? []
+    : [
+        {
+          id: 1,
+          title: 'Buy Milk',
+          completed: true
+        },
+        {
+          id: 2,
+          title: 'Drink Milk',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Prepare Workshop',
+          completed: true
+        },
+        {
+          id: 4,
+          title: 'Run Successful Workshop',
+          completed: false
+        }
+      ];
 
 function nextId(state) {
-  return Math.max(0, ...state.map((i) => i.id)) + 1;
+  return Math.max(0, ...state.map(i => i.id)) + 1;
 }
 
 export default function reducer(state = initial, action) {
   switch (action.type) {
-    case "ADD_TODO": {
+    case 'ADD_TODO': {
       return append(state, {
         id: nextId(state),
         title: action.title,
         completed: false
-      })
+      });
     }
-    case "TOGGLE_TODO": {
-      return updateArrayById(state, action.id,
-        (todo) => update(todo, {completed: !todo.completed})
+    case 'TOGGLE_TODO': {
+      return updateArrayById(state, action.id, todo =>
+        update(todo, { completed: !todo.completed })
       );
     }
-    case "EDIT_TODO": {
-      return updateArrayById(state, action.id,
-        (todo) => update(todo, {title: action.title})
+    case 'EDIT_TODO': {
+      return updateArrayById(state, action.id, todo =>
+        update(todo, { title: action.title })
       );
     }
-    case "REMOVE_TODO": {
-      return state.filter((todo) => todo.id !== action.id);
+    case 'REMOVE_TODO': {
+      return state.filter(todo => todo.id !== action.id);
     }
-    case "SET_ALL": {
-      return state.map((todo) => update(todo, {completed: action.completed}));
+    case 'SET_ALL': {
+      return state.map(todo => update(todo, { completed: action.completed }));
     }
-    case "CLEAR_COMPLETED": {
+    case 'CLEAR_COMPLETED': {
       // eslint-disable-next-line
-      alert("Not implemented - your turn");
+      //  alert("Not implemented - your turn");
+      return state.filter(todo => todo.completed === false);
     }
   }
   return state;
