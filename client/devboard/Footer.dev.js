8258a7d246082abd79049493dfdf6c9b4846782a
Luan <luan.b@stjamesperth.com.au>
2019-12-07 21:25:53 +1100

    added more examples to footer.dev.js. case for none completed, no todos and all completed.

--- client/devboard/Footer.dev.js
+++ client/devboard/Footer.dev.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import devboard from "devboard";
+import devboard from 'devboard';
 
-const definecard = devboard.ns("3. <Footer />");
+const definecard = devboard.ns('3. <Footer />');
 
-import DemoBox from "./DemoBox";
-import Footer from "../components/Footer";
+import DemoBox from './DemoBox';
+import Footer from '../components/Footer';
 
 definecard(
   `
@@ -13,11 +13,30 @@ definecard(
   `
 );
 
-definecard("Sample",
+definecard(
+  'Sample',
   <DemoBox>
     <Footer total={5} completed={3} />
   </DemoBox>
 );
 
+definecard('Add your own here...');
+definecard(
+  'Luans test - none completed',
+  <DemoBox>
+    <Footer total={10} completed={0} />
+  </DemoBox>
+);
+definecard(
+  'Luans test - no todos',
+  <DemoBox>
+    <Footer total={0} completed={0} />
+  </DemoBox>
+);
 
-definecard("Add your own here...");
+definecard(
+  'Luans test - all completed',
+  <DemoBox>
+    <Footer total={10} completed={10} />
+  </DemoBox>
+);
